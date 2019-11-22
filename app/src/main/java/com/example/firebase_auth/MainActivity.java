33dacada9041fd9552f027a369e89310d285b942
SaiBalaji <51410810+SaiBalaji22@users.noreply.github.com>
2019-11-22 09:52:43 +0530

    Developed Register user system and testing it.

---
+++ app/src/main/java/com/example/firebase_auth/MainActivity.java
@@ -0,0 +1,86 @@
+package com.example.firebase_auth;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.regex.Pattern;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, OnCompleteListener<AuthResult> {
+EditText username,password;
+Button signup;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        username=(EditText)findViewById(R.id.editText);
+        password=(EditText)findViewById(R.id.editText2);
+        signup=(Button)findViewById(R.id.button);
+        mAuth = FirebaseAuth.getInstance();
+        signup.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        registeruser();
+    }
+
+    void registeruser()
+    {
+       String email=username.getText().toString().trim();
+       String pass=password.getText().toString().trim();
+       if(email.isEmpty())
+       {
+           username.setError("Email should not be empty");
+           username.requestFocus();
+           return;
+       }
+       if(!Patterns.EMAIL_ADDRESS.matcher(email).matches())
+       {
+           username.setError("Enter a valid email");
+           username.requestFocus();
+           return;
+       }
+
+       if(pass.isEmpty())
+       {
+           password.setError("Password should not be empty");
+           password.requestFocus();
+           return;
+       }
+
+       if(pass.length()<8)
+       {
+           password.setError("Minimum length of password should be 8");
+           password.requestFocus();
+           return;
+       }
+
+       mAuth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener(this);
+
+    }
+
+    @Override
+    public void onComplete(@NonNull Task<AuthResult> task) {
+
+        if(task.isSuccessful())
+        {
+            Toast.makeText(this, "Account created successfully", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+}
